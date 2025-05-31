8c3f5ee4223bc3c1359d4e3a5c38251a117c4b22
xAnTawkentski <xantawkentski@gmail.com>
2025-05-31 15:02:59 +0500

    Create hw1.py

---
+++ lesson-1/homework/hw1.py
@@ -0,0 +1,53 @@
+# Task 1
+a = float(input("Enter the length of the side of the square: "))
+
+# Perimeter and area:
+perimeter = 4 * a
+area = a ** 2
+
+print("Perimeter of the square =", perimeter)
+print("Area of the square =", area)
+_________________________________________________
+# Task 2
+import math
+
+# Ask the user to enter the diameter of the circle
+d = float(input("Enter the diameter of the circle: "))
+
+# Calculate the circumference using the diameter
+L = math.pi * d
+
+print("Circumference of the circle =", L)
+_________________________________________________
+# Task 3
+a = float(input("Enter number a: "))
+b = float(input("Enter number b: "))
+
+# Calculate the average of the two numbers
+average = (a + b) / 2
+
+print("The arithmetic mean of the two given numbers =", average)
+________________________________________________
+# Task 4
+a = float(input("Enter number a: "))
+b = float(input("Enter number b: "))
+
+# Sum of the two given numbers
+sum_ab = a + b
+
+# Product of the two given numbers
+product_ab = a * b
+
+# Square of number a
+a_squared = a ** 2
+
+# Square of number b
+b_squared = b ** 2
+
+print("Sum of the given numbers =", sum_ab)
+print("Product of the given numbers =", product_ab)
+print("Square of number a =", a_squared)
+print("Square of number b =", b_squared)
+
+
+
